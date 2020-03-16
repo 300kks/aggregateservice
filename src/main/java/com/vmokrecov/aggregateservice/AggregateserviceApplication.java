3a2b99a3703c93b9dd7976fd8b914c07d467e498
viktormokrecov <viktormokrecov@gmail.com>
2020-03-16 16:23:31 +0300

    init commit

---
+++ src/main/java/com/vmokrecov/aggregateservice/AggregateserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vmokrecov.aggregateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregateserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregateserviceApplication.class, args);
+	}
+
+}
