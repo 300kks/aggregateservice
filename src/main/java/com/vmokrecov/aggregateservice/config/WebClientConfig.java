25fa01d5e7f7892ee9c4d195d85dc1a955e33785
viktormokrecov <viktormokrecov@gmail.com>
2020-03-30 19:32:28 +0300

    add jaeger filter to webclient

--- src/main/java/com/vmokrecov/aggregateservice/config/WebClientConfig.java
+++ src/main/java/com/vmokrecov/aggregateservice/config/WebClientConfig.java
@@ -1,6 +1,9 @@
 package com.vmokrecov.aggregateservice.config;
 
 import com.vmokrecov.aggregateservice.dto.JwtTokenDTO;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.spring.web.client.TracingExchangeFilterFunction;
+import io.opentracing.contrib.spring.web.client.WebClientSpanDecorator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -8,15 +11,18 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.*;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
+
 @Configuration
 public class WebClientConfig {
 
+    private final Tracer tracer;
     private JwtTokenDTO token;
-
     private AuthConfigurationProperties configuration;
 
-    public WebClientConfig(AuthConfigurationProperties configuration) {
+    public WebClientConfig(AuthConfigurationProperties configuration, Tracer tracer) {
         this.configuration = configuration;
+        this.tracer = tracer;
     }
 
     @Bean
@@ -37,6 +43,8 @@ public class WebClientConfig {
 
     private JwtTokenDTO serverAuth() {
         return WebClient.builder()
+                .filter(new TracingExchangeFilterFunction(tracer,
+                        Collections.singletonList(new WebClientSpanDecorator.StandardTags())))
                 .filter(ExchangeFilterFunctions
                         .basicAuthentication(configuration.getClientId(), configuration.getClientSecret()))
                 .build()
